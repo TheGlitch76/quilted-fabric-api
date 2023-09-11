7334b5973656d7efff5e09071cdfa1c603505242
glitch <glitch.g3431@gmail.com>
2023-09-11 14:38:23 -0500

    Revert move to Quilt Gametest API
    Using Quilt's GameTest API:
    - Makes build times excessively long
    - Provides little/no benefit, as we are only using pre-existing Fabric tests.
    - Adds an additional step to updates which wastes time.
    
    This reverts commit 9895bd97 and subsequent commits migrating tests to the Quilt API.

--- fabric-content-registries-v0/src/testmod/java/net/fabricmc/fabric/test/content/registry/FlammableTest.java
+++ fabric-content-registries-v0/src/testmod/java/net/fabricmc/fabric/test/content/registry/FlammableTest.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2016, 2017, 2018, 2019 FabricMC
- * Copyright 2022 The Quilt Project
+ * Copyright 2022 QuiltMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,21 +17,20 @@
 
 package net.fabricmc.fabric.test.content.registry;
 
-import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
-import org.quiltmc.qsl.testing.api.game.QuiltTestContext;
-
 import net.minecraft.block.Blocks;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.GameTestException;
+import net.minecraft.test.TestContext;
 
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 
 public class FlammableTest {
 	/**
 	 * Regression test for <a href="https://github.com/FabricMC/fabric/issues/2108">FlammableBlockRegistry ignoring tags on first load</a>.
 	 */
-	@GameTest(templateName = QuiltGameTest.EMPTY_STRUCTURE)
-	public void testFlammableTag(QuiltTestContext context) {
+	@GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+	public void testFlammableTag(TestContext context) {
 		if (FlammableBlockRegistry.getDefaultInstance().get(Blocks.SAND).getBurnChance() != 4) {
 			throw new GameTestException("Expected blocks in the sand tag to be flammable!");
 		}
--- fabric-events-interaction-v0/src/testmod/java/net/fabricmc/fabric/test/event/interaction/FakePlayerTests.java
+++ fabric-events-interaction-v0/src/testmod/java/net/fabricmc/fabric/test/event/interaction/FakePlayerTests.java
@@ -17,8 +17,7 @@
 
 package net.fabricmc.fabric.test.event.interaction;
 
-import org.quiltmc.qsl.testing.api.game.QuiltGameTest;
-import org.quiltmc.qsl.testing.api.game.QuiltTestContext;
+import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
@@ -28,6 +27,7 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
@@ -40,8 +40,8 @@ public class FakePlayerTests {
 	/**
 	 * Try placing a sign with a fake player.
 	 */
-	@GameTest(templateName = QuiltGameTest.EMPTY_STRUCTURE)
-	public void testFakePlayerPlaceSign(QuiltTestContext context) {
+	@GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+	public void testFakePlayerPlaceSign(TestContext context) {
 		// This is for Fabric internal testing only, if you copy this to your mod you're on your own...
 
 		BlockPos basePos = new BlockPos(0, 1, 0);
@@ -68,8 +68,8 @@ public class FakePlayerTests {
 	/**
 	 * Try breaking a beehive with a fake player (see {@code BeehiveBlockMixin}).
 	 */
-	@GameTest(templateName = QuiltGameTest.EMPTY_STRUCTURE)
-	public void testFakePlayerBreakBeehive(QuiltTestContext context) {
+	@GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+	public void testFakePlayerBreakBeehive(TestContext context) {
 		BlockPos basePos = new BlockPos(0, 1, 0);
 		context.setBlockState(basePos, Blocks.BEEHIVE);
 		context.spawnEntity(EntityType.BEE, basePos.up());
